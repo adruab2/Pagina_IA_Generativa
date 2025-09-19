a67261c3d7364849199647596ba9d24f16bf4adf
Omar-Cc <U22227800@utp.edu.pe>
2025-09-19 04:40:20 -0500

    feat(index): exportar FormularioHeader desde el índice de componentes

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export { default as Navbar } from './navbar';
 export { default as Header } from './header';
 export { default as Footer } from './footer';
+export { default as FormularioHeader } from './formularioHeader';
 export { default as Formulario } from './formulario';
 export { default as TestimoniosInteractivos } from './testimoniosInteractivos';
 export { default as CourseCard } from './CourseCard';
